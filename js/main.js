1d307981c638ac3444fb4b1f58fb56fa457bc664
Erik Nelson <erik.cse@gmail.com>
2022-10-29 19:23:50 -0700

    Changing links around, adding cats

--- js/main.js
+++ js/main.js
@@ -35,12 +35,30 @@ var keyHandler = function (event) {
   // If complete, add animation class and reset
   if (konamiCode.length === currentKey) {
     currentKey = 0
-    document.getElementById('traichu-running').classList.add('slide')
+    document.getElementById('cat-header').classList.add('slide')
     setTimeout(function () {
-      document.getElementById('traichu-running').classList.remove('slide')
+      document.getElementById('cat-header').classList.remove('slide')
     }, 4250)
   }
 }
 
 // Listen for keydown events
 document.addEventListener('keydown', keyHandler, false)
+
+
+addEventListener('load', (event) => {
+  var headerCats = [
+    'img/p_blanket.gif',
+    'img/p_chair.gif',
+    'img/p_comp.gif', 
+    'img/p_cool.gif', 
+    'img/p_keyboard.gif', 
+    'img/p_laptop.gif', 
+    'img/p_music.gif', 
+    'img/p_noodle.gif', 
+    'img/p_norm.gif', 
+    'img/p_soda.gif'
+  ]
+  var index = Math.floor(Math.random() * headerCats.length);
+  document.getElementById("cat-header").src = headerCats[index]
+});
